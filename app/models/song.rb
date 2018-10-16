2b83f9b29ecd0e087810421f4494ba55dad86e55
George  Pianka <gpianka1@gmail.com>
2018-10-16 03:12:14 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
+  def get_genre_name
+    self.genre.name
+  end
 
+  def drake_made_this
+    self.artist = Artist.new(name: "Drake")
+  end
 end
