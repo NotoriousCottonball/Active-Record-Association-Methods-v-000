f57b9f1bf10f990c23e0a1960d5f0f4429a1441c
George  Pianka <gpianka1@gmail.com>
2018-10-16 04:36:10 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require 'active_record'
 require 'rake'
 
 Dir[File.join(File.dirname(__FILE__), "../app/**/*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../lib/**/*.rb")].each {|f| require f}
 
 DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
 DB = ActiveRecord::Base.connection
